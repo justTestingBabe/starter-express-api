0c4e8f69179af1264cf28ad3256f1f79d7505695
justTestingBabe <138496912+justTestingBabe@users.noreply.github.com>
2023-07-03 20:01:41 +0545

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const app = express()
 app.get('/',(req,res)=>{
-    res.sendFile('home.html',{root:__dirname})
+    // res.sendFile('home.html',{root:__dirname})
+    res.sendFile('home.html')
+    
 })
 
 app.listen(process.env.PORT || 3000)
