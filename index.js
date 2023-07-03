df679ba3c11f2e70151fb21cb6d2bb46b322b693
justTestingBabe <138496912+justTestingBabe@users.noreply.github.com>
2023-07-03 20:10:21 +0545

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require('express')
 const app = express()
 app.get('/',(req,res)=>{
-    // res.sendFile('home.html',{root:__dirname})
-    res.sendFile('/home.html')
+    res.sendFile('home.html',{root:__dirname})
+    // res.sendFile('/home.html')
     
 })
 
